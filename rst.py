c55fe846886d6c6c155be84b2835ef6552262971
Esme Povirk <madewokherd@gmail.com>
2024-01-01 01:09:44 +0000

    Revamp error reporting.

--- rst.py
+++ rst.py
@@ -8,17 +8,36 @@ def _find_first(haystack, needles, start=0):
         index = haystack.find(needle, start)
         if index != -1 and (result == -1 or index < result):
             result = index
-            result_str = needle
+            result_str = haystack[index:index+len(needle)] # preserves source location
     return result_str, result
 
-def _update_location(string, line, col):
-    lines = string.count('\n')
-    if lines:
-        line += lines
-        col = len(string.rsplit('\n', 1)[1])
-    else:
-        col += len(string)
-    return line, col
+class StringLocation(str):
+    __slots__ = ['line', 'column', 'filename', 'file_contents']
+
+    def __new__(cls, s, filename, line=1, column=1, file_contents=None):
+        self = str.__new__(cls, s)
+        self.filename = filename
+        self.line = line
+        self.column = column
+        if file_contents is None:
+            self.file_contents = s
+        else:
+            self.file_contents = file_contents
+        return self
+
+    def __getitem__(self, index):
+        s = super().__getitem__(index)
+        if isinstance(index, slice):
+            index = index.start
+        prefix = super().__getitem__(slice(0, index))
+        lines = prefix.count('\n')
+        if lines:
+            line = self.line + lines
+            col = len(prefix.rsplit('\n', 1)[1])+1
+        else:
+            line = self.line
+            col = self.column + len(prefix)
+        return StringLocation(s, self.filename, line, col, self.file_contents)
 
 class TemplatingState:
     outfile = None
@@ -32,21 +51,22 @@ class TemplatingState:
         self.variables['lb'] = '{'
         self.variables['rb'] = '}'
 
-    def report_error(self, full_source, filename, line, column, message):
+    def report_error(self, source_string, message):
         print(f"Error: {message}")
-        print(f"in file {filename}, line {line}, column {column}:")
-        print(full_source.splitlines()[line-1])
-        print(' '*(column - 1) + '^')
+        if isinstance(source_string, StringLocation):
+            print(f"in file {source_string.filename}, line {source_string.line}, column {source_string.column}:")
+            print(source_string.file_contents.splitlines()[source_string.line-1])
+            print(' '*(source_string.column - 1) + '^')
+        else:
+            print(f"in string: {source_string}")
         sys.exit(1)
 
     def process_filename(self, filename):
         with open(filename, 'r') as f:
-            self.process(f.read(), filename=filename)
+            self.process(StringLocation(f.read(), filename))
 
-    def process(self, source, filename='', line=1, col=1, full_source=None):
+    def process(self, source):
         index = 0
-        if full_source is None:
-            full_source = source
 
         while True:
             next_directive, next_index = _find_first(source, ('<%', '{{'), index)
@@ -54,26 +74,24 @@ class TemplatingState:
                 self.outfile.write(source[index:])
                 return
             self.outfile.write(source[index:next_index])
-            line, col = _update_location(source[index:next_index], line, col)
             if next_directive == '{{':
                 end_index = source.find('}}', next_index)
                 if end_index == -1:
-                    self.report_error(full_source, filename, line, col, "'{{' without matching '}}'")
+                    self.report_error(next_directive, "'{{' without matching '}}'")
                 varname = source[next_index+2:end_index]
                 if '{{' in varname:
-                    self.report_error(full_source, filename, line, col, "variable name may not contain '{{'")
+                    self.report_error(varname[varname.index('{{')], "variable name may not contain '{{'")
                 if varname not in self.variables:
-                    self.report_error(full_source, filename, line, col, f"undefined variable: {varname}")
+                    self.report_error(varname, f"undefined variable: {varname}")
                 self.outfile.write(self.variables[varname])
-                line, col = _update_location(source[next_index:end_index+2], line, col)
                 index = end_index+2
             elif next_directive == '<%':
                 varname_end = source.find('>', next_index)
                 if varname_end == -1:
-                    self.report_error(full_source, filename, line, col, "'<%' without matching '>'")
+                    self.report_error(next_directive, "'<%' without matching '>'")
                 varname = source[next_index+2:varname_end]
                 if '{{' in varname:
-                    self.report_error(full_source, filename, line, col, "variable name may not contain '{{'")
+                    self.report_error(varname[varname.index('{{')], "variable name may not contain '{{'")
 
                 #not sure why anyone would nest variable declarations, but just in case
                 nest_count = 1
@@ -87,21 +105,17 @@ class TemplatingState:
                     elif found_tag == close_tag:
                         nest_count -= 1
                     else:
-                        self.report_error(full_source, filename, line, col, "'<%{varname}>' without matching '</%{varname}>'")
-
-                #advance line, col to the start of the variable contents so we can pass them in directly
-                line, col = _update_location(source[next_index:varname_end+1], line, col)
+                        self.report_error(next_directive, f"'<%{varname}>' without matching '</%{varname}>'")
 
                 varstream = io.StringIO()
                 prev_outfile = self.outfile
                 self.outfile = varstream
-                self.process(source[varname_end+1:close_index], filename, line, col, full_source)
+                self.process(source[varname_end+1:close_index])
                 self.outfile = prev_outfile
 
                 self.variables[varname] = varstream.getvalue()
 
                 index = close_index + len(close_tag)
-                line, col = _update_location(source[varname_end+1:index], line, col)
 
 def main():
     st = TemplatingState()
